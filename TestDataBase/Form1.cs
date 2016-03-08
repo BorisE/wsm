45a213cc3ea538dcddd64469fd149a62824a466c
BorisE <e_b_s@inbox.ru>
2016-03-08 11:17:32 +0300

    1.9.72db_1

---
+++ TestDataBase/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Data;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestDataBase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string baseName = "SensorData.db3";
+
+            SQLiteConnection.CreateFile(baseName);
+
+            SQLiteFactory factory = (SQLiteFactory)DbProviderFactories.GetFactory("System.Data.SQLite");
+            using (SQLiteConnection connection = (SQLiteConnection)factory.CreateConnection())
+            {
+                connection.ConnectionString = "Data Source = " + baseName;
+                connection.Open();
+
+                using (SQLiteCommand command = new SQLiteCommand(connection))
+                {
+                    command.CommandText = @"CREATE TABLE [Sensors] (
+                    [id] integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+                    [name] char(100) NOT NULL,
+                    [family] char(100) NOT NULL,
+                    [age] int NOT NULL,
+                    [profession] char(100) NOT NULL
+                    );";
+                    command.CommandType = CommandType.Text;
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
